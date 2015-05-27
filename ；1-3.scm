9c62f63f302594fc743ef3cfe111e66034b76261
thirtiseven lee <ntlihy@gmail.com>
2015-05-27 20:22:07 +0800

    update

--- ；1-3.scm
+++ ；1-3.scm
@@ -1,10 +1,3 @@
 ；1-3.scm
-(define (SumOfMaxtwo alice bob carol)
-		cond (> alice bob) (+ alice (cond ((> bob carol) bob  )
-									 cond ((> carol bob) carol)))
-		cond (> bob alice) (+ bob   (cond ((> alice carol) alice)
-									 cond ((> carol alice) bob)))
-		cond (> carol bob) (+ carol (cond ((> alice carol) alice)
-									 cond ((> carol alice) carol))))
-
-(SumOfMaxtwo 15 18 233)
+(define (sum-of-squared-largest-two a b c) 
+   (- (+ (square a) (square b) (square c)) (square (min a b c)))) 
